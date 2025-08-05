4f4982e34fa7f1a64633f1cd86fc65fab76b4792
bbedevian <brettbedevian@gmail.com>
2025-08-05 19:52:11 -0400

    add multiply

--- _js/calculator.js
+++ _js/calculator.js
@@ -14,10 +14,19 @@ const calculator = (() => {
 
         return num1 - num2;
     }
+    
+     function multiply(num1, num2) {
+        if(isNaN(num1) || isNaN(num2)) {
+            return "not valid numbers";
+        }
+
+        return num1 * num2;
+    }
 
     return {
         add,
-        subtract
+        subtract,
+        multiply
     }
 })();
 
